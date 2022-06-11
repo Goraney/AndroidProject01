6f2f27fc6c10c0b348fc790c5a22dadf5638789b
Goraney <btok92@gmail.com>
2022-06-12 01:56:15 +0900

    BitmapPool 추가

---
+++ Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/BitmapPool.java
@@ -0,0 +1,28 @@
+package kr.ac.tukorea.sgp02.u2019182020.wanderer;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.HashMap;
+
+public class BitmapPool {
+    private static HashMap<Integer, Bitmap> bitmaps = new HashMap<>();
+    public static BitmapFactory.Options opts;
+    static {
+        opts = new BitmapFactory.Options();
+        opts.inScaled = false;
+    }
+
+    public static Bitmap get(int mipmapResId) {
+        Bitmap bitmap = bitmaps.get(mipmapResId);
+
+        if (bitmap == null) {
+            Resources res = GameView.view.getResources();
+            bitmap = BitmapFactory.decodeResource(res, mipmapResId, opts);
+            bitmaps.put(mipmapResId, bitmap);
+        }
+
+        return bitmap;
+    }
+}
