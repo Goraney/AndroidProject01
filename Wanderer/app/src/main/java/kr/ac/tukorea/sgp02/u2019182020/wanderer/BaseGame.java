36c1ed031e889c8a1708864edf28525bb32b8574
Goraney <btok92@gmail.com>
2022-06-11 23:40:00 +0900

    BaseGame update, draw, layer 구현

--- Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/BaseGame.java
+++ Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/BaseGame.java
@@ -1,6 +1,9 @@
 package kr.ac.tukorea.sgp02.u2019182020.wanderer;
 
 import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
 
 public class BaseGame {
     protected static BaseGame singleton;
@@ -15,20 +18,52 @@ public class BaseGame {
         singleton = null;
     }
 
+    protected ArrayList<ArrayList<GameObject>> layers;
+
     public void init() {
         elapsedTime = 0;
+    }
+
+    protected void initLayers(int count) {
+        layers = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            layers.add(new ArrayList<>());
+        }
     }
 
     public void update(int elapsedNanos) {
         frameTime = (float) (elapsedNanos / 1_000_000_000f);
         elapsedTime += frameTime;
 
-        // 오브젝트 업데이트할것
+        for (ArrayList<GameObject> gameObjects : layers) {
+            for (GameObject gobj : gameObjects) {
+                gobj.update();
+            }
+        }
     }
 
     public void draw(Canvas canvas) {
-        // 오브젝트 드로우할것
+        for (ArrayList<GameObject> gameObjects : layers) {
+            for (GameObject gobj : gameObjects) {
+                gobj.draw(canvas);
+            }
+        }
     }
 
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
 
+    public ArrayList<GameObject> objectsAt(int layerIndex) {
+        return layers.get(layerIndex);
+    }
+
+    public int objectCount() {
+        int count = 0;
+
+        for (ArrayList<GameObject> gameObjects : layers) {
+            count += gameObjects.size();
+        }
+        return count;
+    }
 }
