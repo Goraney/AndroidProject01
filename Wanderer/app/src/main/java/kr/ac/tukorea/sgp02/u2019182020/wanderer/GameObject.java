33eb5effc20be926e009dee540cd13217a6580a5
Goraney <btok92@gmail.com>
2022-06-11 23:03:50 +0900

    GameObject 추가

---
+++ Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/GameObject.java
@@ -0,0 +1,8 @@
+package kr.ac.tukorea.sgp02.u2019182020.wanderer;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
