7609bcdaac1e600464de6e4fc990186a2797d557
Goraney <btok92@gmail.com>
2022-06-13 17:48:52 +0900

    Portal 구현

--- Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/game/Portal.java
+++ Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/game/Portal.java
@@ -1,4 +1,37 @@
 package kr.ac.tukorea.sgp02.u2019182020.wanderer.game;
 
-public class Portal {
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
+import kr.ac.tukorea.sgp02.u2019182020.wanderer.R;
+import kr.ac.tukorea.sgp02.u2019182020.wanderer.framework.BitmapPool;
+import kr.ac.tukorea.sgp02.u2019182020.wanderer.framework.BoxCollidable;
+import kr.ac.tukorea.sgp02.u2019182020.wanderer.framework.Sprite;
+
+public class Portal extends Sprite implements BoxCollidable {
+    protected RectF collisionBox = new RectF();
+
+    public Portal(float x, float y, float w, float h) {
+        super(x, y, w, h, R.mipmap.portal);
+        this.x = x;
+        this.y = y;
+        setDstRect(w, h);
+        collisionBox.set(x - w / 2, y - h / 2, x + w / 2, y + h / 2);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public RectF getBoundingRect() {
+        return collisionBox;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, null, dstRect, null);
+    }
+
 }
