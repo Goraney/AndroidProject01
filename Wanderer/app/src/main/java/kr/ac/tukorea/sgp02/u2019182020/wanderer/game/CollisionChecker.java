0a029417320d199610b16187cde0fd8c7a74861a
Goraney <btok92@gmail.com>
2022-06-13 18:57:34 +0900

    Player - Portal 충돌체크 (충돌처리 미적용)

--- Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/game/CollisionChecker.java
+++ Wanderer/app/src/main/java/kr/ac/tukorea/sgp02/u2019182020/wanderer/game/CollisionChecker.java
@@ -1,7 +1,14 @@
 package kr.ac.tukorea.sgp02.u2019182020.wanderer.game;
 
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 
+import androidx.appcompat.app.AlertDialog;
+
+import java.util.ArrayList;
+
+import kr.ac.tukorea.sgp02.u2019182020.wanderer.framework.BoxCollidable;
+import kr.ac.tukorea.sgp02.u2019182020.wanderer.framework.CollisionHelper;
 import kr.ac.tukorea.sgp02.u2019182020.wanderer.framework.GameObject;
 
 public class CollisionChecker implements GameObject {
@@ -14,7 +21,17 @@ public class CollisionChecker implements GameObject {
     @Override
     public void update() {
         MainScene game = MainScene.get();
+        ArrayList<GameObject> portals = game.objectsAt(MainScene.Layer.portal.ordinal());
+        for (GameObject portal : portals)
+            if (CollisionHelper.collides(player, (BoxCollidable) portal)) {
+                Portal port = (Portal) portal;
+                if (!port.valid) {
+                    continue;
+                }
+                else {
 
+                }
+            }
     }
 
     @Override
